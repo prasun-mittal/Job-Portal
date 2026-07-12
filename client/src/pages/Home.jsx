bba6bed116f4a0df984d481e6b9cf7b8e2e8669d
Prasun Mittal <prasunmittal09@gmail.com>
2026-07-12 13:34:03 +0530

    Implement hero section with title and location search

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <NavBar />
+        <Hero />
+    </div>
   )
 }
 
