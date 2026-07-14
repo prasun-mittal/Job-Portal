3f8e7c135f2968cf853149cb063a352702f49a21
Prasun Mittal <prasunmittal09@gmail.com>
2026-07-14 16:10:47 +0530

    Add responsive job listing filters and job cards

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,11 +1,14 @@
-import React,{useContext} from 'react'
+import React,{useContext,useState} from 'react'
 import { AppContext } from '../context/AppContext'
-import { assets, JobCategories, JobLocations, jobsData } from '../assets/assets';
+import { assets, JobCategories, JobLocations } from '../assets/assets';
 import JobCard from './JobCard';
 
 const JobListing = () => {
 
-  const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
+  const { isSearched, searchFilter, setSearchFilter,jobs } = useContext(AppContext);
+  
+  const [showFilter,setShowFilter] = useState(false)
+
   return (
     <div className='container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
       {/* Sidebar  */}
@@ -34,8 +37,13 @@ const JobListing = () => {
             </>
           )
         }
+
+        <button onClick={e => setShowFilter(prev => !prev)} className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>
+          {showFilter ? "Close":"Filters"}
+        </button>
+
         {/* Category Filter */}
-        <div className='max-lg:hidden'>
+        <div className={showFilter? "" : 'max-lg:hidden'}>
           <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
           <ul className='space-y-4 text-gray-400'>
               {
@@ -50,7 +58,7 @@ const JobListing = () => {
         </div>
 
         {/*   Location Filter */}
-        <div className='max-lg:hidden'>
+        <div className={showFilter? "" : 'max-lg:hidden'}>
           <h4 className='font-medium text-lg py-4 pt-12'>Search by Location</h4>
           <ul className='space-y-4 text-gray-400'>
               {
@@ -70,7 +78,7 @@ const JobListing = () => {
               <h3 className='font-medium text-3xl py-2' id='job-list'>Latest jobs</h3>
               <p className='mb-8'>Get your desired job from top companies</p>
               <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
-                  {jobsData.map((job,index)=>(
+                  {jobs.map((job,index)=>(
                       <JobCard key={index} job={job}/>
                   ))}
               </div>
