2f7828e67d5db79f3ed26cd01bf609fda6064f81
Prasun Mittal <prasunmittal09@gmail.com>
2026-07-19 15:22:30 +0530

    Implement job details page with related jobs section

--- client/src/pages/ApplyJobs.jsx
+++ client/src/pages/ApplyJobs.jsx
@@ -6,6 +6,8 @@ import Loading from '../components/Loading'
 import NavBar from '../components/NavBar'
 import kconvert from 'k-convert';
 import moment from 'moment';
+import JobCard from '../components/JobCard';
+import Footer from '../components/Footer';
 
 const ApplyJobs = () => {
 
@@ -19,7 +21,7 @@ const ApplyJobs = () => {
     const data = jobs.filter(job => job._id === id)
     if (data.length !==0) {
       setJobData(data[0])
-      console.log(data[0]);
+      // console.log(data[0]);
     }
   }
 
@@ -65,8 +67,25 @@ const ApplyJobs = () => {
               <p className='mt-1 text-gray-600'>Posted {moment(jobData.date).fromNow()}</p>
             </div>
           </div>
+
+            <div className='flex flex-col lg:flex-row justify-between items-start'>
+              <div className='w-full lg:w-2/3'>
+                  <h2 className='font-bold text-2xl mb-4 '>Job description</h2>
+                  <div className='rich-text' dangerouslySetInnerHTML={{__html:jobData.description}}></div>
+                  <button className='bg-blue-600 p-2.5 px-10 text-white rounded cursor-pointer mt-10'>Apply Now</button>
+              </div>
+              {/* Right section More Jobs */}
+              <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
+                  <h2>More Jobs from {jobData.companyId.name}</h2>
+                  {jobs.filter(job=> job._id !== jobData._id && job.companyId._id === jobData.companyId._id)
+                  .filter(job => true).slice(0,4)
+                  .map((job,index)=> <JobCard key={index} job={job} />)}
+              </div>
+            </div>
+
         </div>
       </div>
+      <Footer />
     </>
   ) : (
     <Loading />
