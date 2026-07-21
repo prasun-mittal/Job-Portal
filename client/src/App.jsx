60bfd31115589527476cb43c319f7e13924634d2
Prasun Mittal <prasunmittal09@gmail.com>
2026-07-21 19:47:40 +0530

    Design recruiter login and signup popup UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/home'
 import ApplyJobs from './pages/ApplyJobs'
 import Application from './pages/Application'
+import RecruiterLogin from './components/RecruiterLogin'
+import { AppContext } from './context/AppContext'
 
 const App = () => {
+
+  const {showRecruiterLogin} = useContext(AppContext)
+
   return (
     <div>
+    { showRecruiterLogin && <RecruiterLogin />}
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/apply-Job/:id' element={<ApplyJobs/>}/>
