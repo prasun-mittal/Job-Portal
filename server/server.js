3dc7c63f8db76bcc95eca0abdc97834a3e1d21fc
Prasun Mittal <prasunmittal09@gmail.com>
2026-07-27 21:37:17 +0530

    Setup backend server with Express and MongoDB Atlas

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js'
+
+// Initialize Express
+const app = express()
+
+// connect to database
+await connectDB()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/',(req,res)=> res.send("API working"))
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=> {
+    console.log(`Server is Running on PORT: ${PORT}`);
+})
