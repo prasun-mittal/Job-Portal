45b04249d4ac11c65243dfc8b388dffa2fe6f121
Prasun Mittal <prasunmittal09@gmail.com>
2026-08-01 20:09:52 +0530

    Integrate company login with Axios and JWT handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import ManageJobs from './pages/ManageJobs'
 import ViewApplication from './pages/ViewApplication'
 import AddJob from './pages/AddJob'
 import 'quill/dist/quill.snow.css'
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 
@@ -18,6 +20,7 @@ const App = () => {
   return (
     <div>
     { showRecruiterLogin && <RecruiterLogin />}
+    <ToastContainer />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/apply-Job/:id' element={<ApplyJobs/>}/>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,6 +5,8 @@ export const AppContext = createContext()
 
 export const AppContextProvider =(props) => {
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const [searchFilter,setSearchFilter] = useState({
         title:'',
         location:''
@@ -15,6 +17,9 @@ export const AppContextProvider =(props) => {
     const [jobs,setJobs] = useState([]);
 
     const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
+
+    const [companyToken,setCompanyToken] = useState(null)
+    const [companyData,setCompanyData] = useState(null)
 
     // Function to fetch job data
     const fetchJobs = async ()=>{
@@ -30,6 +35,9 @@ export const AppContextProvider =(props) => {
             isSearched,setIsSearched,
             jobs,setJobs,
             showRecruiterLogin,setShowRecruiterLogin,
+            companyData,setCompanyData,
+            companyToken,setCompanyToken,
+            backendUrl,
     }
 
     return (<AppContext.Provider value={value}>
