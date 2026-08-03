bbbf6b283d51db564b4c1e96cbe42cec3a6631bb
Prasun Mittal <prasunmittal09@gmail.com>
2026-08-03 19:18:13 +0530

    add jobs and manage jobs page connect with backend

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Dashboard = () => {
 
     const navigate=useNavigate()
+
+    const {companyData} = useContext(AppContext)
 
   return (
     <div className='min-h-screen'>
@@ -13,10 +16,11 @@ const Dashboard = () => {
         <div className='shadow py-4'>
             <div className='px-5 flex justify-between items-center'>
                 <img onClick={e=>navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" ></img>
+                {companyData && (
                 <div className='flex items-center gap-3'>
-                    <p className='max-sm:hidden'>Welcome, GreateStack</p>
+                    <p className='max-sm:hidden'>Welcome, {companyData.name}</p>
                     <div className='relative group'>
-                        <img className='w-8 border rounded-full' src={assets.company_icon} alt="" ></img>
+                        <img className='w-8 ' src={companyData.image} alt="" ></img>
                         <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                             <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
                                 <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
@@ -24,6 +28,8 @@ const Dashboard = () => {
                         </div>
                     </div>
                 </div>
+                )}
+                
             </div>
         </div>
 
