8bca951ba26e62ab013dcd040538a92ef6fea09a
Prasun Mittal <prasunmittal09@gmail.com>
2026-08-10 22:40:44 +0530

    Implemented recruiter applicants dashboard

--- client/src/pages/ApplyJobs.jsx
+++ client/src/pages/ApplyJobs.jsx
@@ -126,7 +126,7 @@ const ApplyJobs = () => {
 
             <div className='flex flex-col lg:flex-row justify-between items-start'>
               <div className='w-full lg:w-2/3'>
-                  <h2 className='font-bold text-2xl mb-4 '>Job description</h2>
+                  <h2 className='font-bo ld text-2xl mb-4 '>Job description</h2>
                   <div className='rich-text' dangerouslySetInnerHTML={{__html:jobData.description}}></div>
                   <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded cursor-pointer mt-10'>{isAlreadyApplied?'Already Applied' : 'Apply Now'}</button>
               </div>
--- client/src/pages/ViewApplication.jsx
+++ client/src/pages/ViewApplication.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets, viewApplicationsPageData } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import Loading from '../components/Loading'
 
 const ViewApplication = () => {
-  return (
+
+  const {backendUrl, companyToken} = useContext(AppContext)
+
+  const [applicants,setApplicants] = useState(false)
+
+  // function to fetch company job Application data
+  const fetchCompanyJobApplications = async () => {
+    
+    try {
+      
+      const {data} = await axios.get(backendUrl+'/api/company/applicants',
+        {headers: {token: companyToken}}
+      )
+
+      if (data.success) {
+        setApplicants(data.applications.reverse())
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+
+  }
+  useEffect(()=>{
+    if (companyToken) {
+      fetchCompanyJobApplications()
+    }
+  },[companyToken])
+  return applicants ? applicants.length === 0 ? (<div></div>) : (
+
     <div className='conatiner mx-auto p-4'>
 
       <div>
@@ -18,17 +53,17 @@ const ViewApplication = () => {
             </tr>
           </thead>
           <tbody>
-            {viewApplicationsPageData.map((applicant,index)=>(
+            {applicants.filter(item => item.jobId && item.userId).map((applicant,index)=>(
               <tr key={index} className='text-gray-700'>
                 <td className='py-2 px-4 border-b text-center'>{index+1}</td>
-                <td className='py-2 px-4 border-b text-center flex'>
-                  <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.imgSrc} alt="" />
-                  <span>{applicant.name}</span>
+                <td className='py-2 px-4 border-b text-center flex items-center'>
+                  <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.userId.image} alt="" />
+                  <span>{applicant.userId.name}</span>
                 </td>
-                <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.jobTitle}</td>
-                <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.location}</td>
+                <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.jobId.title}</td>
+                <td className='py-2 px-4 border-b max-sm:hidden'>{applicant.jobId.location}</td>
                 <td className='py-2 px-4 border-b'>
-                  <a href='' target='_blank'
+                  <a href={applicant.userId.resume} target='_blank'
                     className='bg-blue-50 text-blue-400 px-3 py-1 rounded inline-flex gap-2 items-center '
                   >
                     Resume <img src={assets.resume_download_icon} alt="" />
@@ -50,7 +85,7 @@ const ViewApplication = () => {
       </div>
 
     </div>
-  )
+  ) : <Loading />
 }
 
 export default ViewApplication
